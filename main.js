71f1211948be646fc39d00e25a918102fdc419c9
vitKama <69966781+vitKama@users.noreply.github.com>
2023-07-20 13:32:50 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express") // This is how you load modules in Node JS
-const app = express() // Initialize Express
-
-app.get("/", (req, res) => {
-    res.send("Express JS server example") // The information it sends to someone who opens the website
-})
-
-app.listen(3000) // Listen on port 3000
